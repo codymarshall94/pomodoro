82dc7a4734c87e1328eb156e8a5e7e3a87c3aae3
Cody Marshall <codymstuntsx@gmail.com>
2024-02-09 22:22:00 -0800

    Moved youtubeExtract to folder

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const extractYoutubeId = (link: string) => {
+  // Regular expression to match YouTube video URL
+  const youtubeRegex =
+    /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]+)$/;
+
+  // Check if the link matches the YouTube URL pattern
+  const isYoutubeLink = youtubeRegex.test(link);
+
+  if (isYoutubeLink) {
+    // Extract the video ID
+    const match = link.match(
+      /(?:\?v=|\/embed\/|\/\d\/|\/vi\/|\/v\/|https:\/\/youtu.be\/)([a-zA-Z0-9_-]{11})/
+    );
+    return match ? match[1] : null;
+  } else {
+    return null;
+  }
+};
