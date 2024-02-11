524284c3f2faf46f203f55a6b5bddcaa801bc757
Cody Marshall <codymstuntsx@gmail.com>
2024-02-11 14:52:55 -0800

    Component overhaul

--- components/widgets/notepad/Notepad.tsx
+++ components/widgets/notepad/Notepad.tsx
@@ -1,22 +1,35 @@
 "use client";
 
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-
-export interface TodoItemProps {
-  id: string;
-  text: string;
-  completed: boolean;
-}
+import { useWidgetsStore } from "@/store/widgetStore";
+import { useEffect, useState } from "react"; // Import useState as well
 
 const Notepad = () => {
-  return (
-    <div className="flex flex-col bg-card rounded-md p-4 gap-4 h-min opac">
-      <h3 className="text-primary font-bold">Notepad</h3>
-      <Textarea />
-    </div>
+  const initialNotepadContent = useWidgetsStore(
+    (state) =>
+      state.widgets.find((widget) => widget.id === "notepad")?.content || ""
   );
+
+  const [notepadContent, setNotepadContent] = useState(initialNotepadContent);
+  const updateWidgetContent = useWidgetsStore(
+    (state) => state.updateWidgetContent
+  );
+
+  useEffect(() => {
+    setNotepadContent(initialNotepadContent);
+  }, [initialNotepadContent]);
+
+  const handleNotepadChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setNotepadContent(event.target.value);
+  };
+
+  useEffect(() => {
+    updateWidgetContent("notepad", notepadContent);
+  }, [notepadContent, updateWidgetContent]);
+
+  return <Textarea value={notepadContent} onChange={handleNotepadChange} />;
 };
 
 export default Notepad;
